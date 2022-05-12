9f30689fd28193e3688763cb709f5f799786d4c5
Vlad Minaev <minaev@haulmont.com>
2022-05-13 02:11:29 +0400

    Add ide-toolbox

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews} from "./dev";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <DevSupport ComponentPreviews={ComponentPreviews}>
+            <App/>
+        </DevSupport>
+    </React.StrictMode>
 )
