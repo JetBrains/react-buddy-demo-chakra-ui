ee0a3e5d5244accf36170623ee7f031d11ee8890
Lev Palkin <palkinli@haulmont.com>
2022-11-18 19:17:23 +0400

    format with prettier palette.tsx

--- src/dev/previews.tsx
+++ src/dev/previews.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox';
-import {PaletteTree} from './palette';
+import PaletteTree from './palette';
 import App from "../App";
 
 const ComponentPreviews = () => {
